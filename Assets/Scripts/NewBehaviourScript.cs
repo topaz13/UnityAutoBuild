cff74936772ef7a1a2af5fcac4c3992a76bf7746
topaz <haruki.30.30.11@gmail.com>
2022-11-08 16:41:59 +0900

    add settings;2A

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] Image _image;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            float r = Random.Range(0.0f, 1.0f);
+            Color c = new Color(r, r, r);
+            _image.color = c;
+        }
     }
 }
